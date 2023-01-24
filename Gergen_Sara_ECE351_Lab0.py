8fbe614921bcd9b2c204da2fc67e40f077ee0ab2
Sara Gergen <122953768+CheeseLoop2@users.noreply.github.com>
2023-01-23 22:32:29 -0800

    Add files via upload

---
+++ Gergen_Sara_ECE351_Lab0.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 23 17:55:43 2023
+
+@author: Sara
+"""
+
+################################################################
+#                                                              #
+# Sara Gergen                                                  #
+# 351-53                                                       #
+# Lab 0                                                        #
+# 1/24/2023                                                    #
+#                                                              #
+#                                                              #
+################################################################
+
+lab = "ECE 351 - Lab 0"
+title = "Questions and Deliverables\n"
+
+
+print(lab)
+print(title)
+
+
+print(" 1. What did you do over last summer, or what are you planning to do this upcoming summer?")
+
+print("\n This upcoming summer, I would like to learn many new skills; I plan to start more projects through a maker space called Gizmo. Another skill that I would like to broaden is my use of computers. I may install a Linux operating system on my computer to learn how a computer operates on a more basic level. Additionally, I would like to apply for possible internships to further my career.")
+
+print("\n 2. What do you personally want to get out of this lab?")
+
+print("\n I personally am excited to be able to comprehend Latex on a deeper level; the last time I attempted to learn it, I was frustrated and gave up since there was no purpose for me to use it. However, this class will allow me to discover a new way to format papers professionally. By messing around with the formatting and looking up the formatting on the provided cheat sheet, I was able to understand it better. I am also exhilarated to be able to learn python. I previously taught myself some python over break, and I am excited to not only test my knowledge, but also learn more about python.")
+
+
