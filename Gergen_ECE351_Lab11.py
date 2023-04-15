46b3872911b4d68876c4cf873ee93d18c7fca715
Sara Gergen <122953768+CheeseLoop2@users.noreply.github.com>
2023-04-14 21:52:54 -0700

    Add files via upload

---
+++ Gergen_ECE351_Lab11.py
@@ -0,0 +1,19 @@
+################################################################
+#                                                              #
+# Sara Gergen                                                  #
+# 351-53                                                       #
+# Lab 11                                                     #
+# Due: 4/11/2023                                                #
+#                                                              #
+################################################################
+
+import control as con
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy as sp
+import scipy.signal as sig
+import pandas as pd
+import control
+import time
+from scipy.fftpack import fft , fftshift
+import math
